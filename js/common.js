fb94eeb014ef482c7320abac8278f25a6f576b77
andreyka_g <andreyka_g@ukr.net>
2015-01-29 16:58:48 +0200

    fixed

--- js/common.js
+++ js/common.js
@@ -17,11 +17,14 @@ head.ready(function() {
 		console.log($('body').html());
 		var ul = $( ".out .lside .lside__nav ul" );
 		var a = $( ".out .lside .lside__nav ul li a" );
+		var nav = $('.lside')
 		ul.mouseover(function() {
 			ul.animate({width: 150}, "fast");
+			nav.animate({width: 160}, "fast");
 		setInterval(	function() {
 		 	ul.queue(function () {
 	  	      a.addClass("open"); 
-	  	    })}, 1000);
+	  	    })}, 800);
+		 // a.addClass("open"); 
 	  });
 });
