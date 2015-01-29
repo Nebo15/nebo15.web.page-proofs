ed35301e48d977477096bd090c9c818c728c8f21
andreyka_g <andreyka_g@ukr.net>
2015-01-29 16:15:29 +0200

    fixed

--- js/common.js
+++ js/common.js
@@ -14,5 +14,14 @@ head.ready(function() {
 	// $(window).scroll(function(){
 	//     scrollFixedElements()
 	// });
-	console.log($('body').html());
+		console.log($('body').html());
+		var ul = $( ".out .lside .lside__nav ul" );
+		var a = $( ".out .lside .lside__nav ul li a" );
+		ul.mouseover(function() {
+			ul.animate({width: 150}, "fast");
+		setInterval(	function() {
+		 	ul.queue(function () {
+	  	      a.addClass("open"); 
+	  	    })}, 1000);
+	  });
 });
